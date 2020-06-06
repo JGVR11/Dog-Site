0ae170b2747295a3920ea0306fc2d59720fdbb8c
Juan Vasquerz <jgvasquez@my.milligan.edu>
2020-06-05 21:56:26 -0400

    Added functionality to the dogs photo album

---
+++ main.js
@@ -0,0 +1,45 @@
+document.getElementById('previous-arrow').innerHTML = '<';
+document.getElementById('next-arrow').innerHTML = '>';
+const dogPhotos = ['Resources/Smiley-Dog.jpg', 'Resources/husky.jpg', 'Resources/Dogs1.jpg', 'Resources/dogs-playing.jpg', 'Resources/dog-under-blanket.jpg'];
+
+let photoAlbum = document.getElementById('photo-album');
+let photoCount = document.getElementById('photo-count');
+let previousArrow = document.getElementById('previous-arrow');
+let nextArrow = document.getElementById('next-arrow');
+
+
+const getNextPhoto = () => {
+   if (photoCount.innerHTML === '1 of 5') {
+       photoAlbum.setAttribute('src', dogPhotos[1]);
+       photoCount.innerHTML = '2 of 5';
+   } else if (photoCount.innerHTML === '2 of 5') {
+       photoAlbum.setAttribute('src', dogPhotos[2]);
+       photoCount.innerHTML = '3 of 5';
+   } else if (photoCount.innerHTML === '3 of 5') {
+       photoAlbum.setAttribute('src', dogPhotos[3]);
+       photoCount.innerHTML = '4 of 5';
+   }  else if (photoCount.innerHTML === '4 of 5') {
+       photoAlbum.setAttribute('src', dogPhotos[4]);
+       photoCount.innerHTML = '5 of 5';
+   }
+    
+};
+
+const getPreviousPhoto = () => {
+    if (photoCount.innerHTML === '2 of 5') {
+        photoAlbum.setAttribute('src', dogPhotos[0]);
+        photoCount.innerHTML = '1 of 5';
+    } else if (photoCount.innerHTML === '3 of 5') {
+        photoAlbum.setAttribute('src', dogPhotos[1]);
+        photoCount.innerHTML = '2 of 5';
+    } else if (photoCount.innerHTML === '4 of 5') {
+        photoAlbum.setAttribute('src', dogPhotos[2]);
+        photoCount.innerHTML = '3 of 5';
+    } else if (photoCount.innerHTML === '5 of 5') {
+        photoAlbum.setAttribute('src', dogPhotos[3]);
+        photoCount.innerHTML = '4 of 5';
+    }
+};
+
+nextArrow.onclick = getNextPhoto;
+previousArrow.onclick = getPreviousPhoto;
